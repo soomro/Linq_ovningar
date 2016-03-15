e6785c1ac93d8564b5426fc8bcc5b55ac1e20d61
Waseem Soomro <ws@waseemsoomro.com>
2016-03-15 11:49:04 +0100

    until 6 tasks
    until 6 tasks

--- Linq_ovningar/Program.cs
+++ Linq_ovningar/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,30 +7,99 @@ using System.Threading.Tasks;
 
 namespace Linq_ovningar
 {
-    class Program
+    static class Program
     {
+        public static void Main(string[] args)
+        {
+            string space = "\n\n\n\n";
+            List<Employee> employees = null;
+
+            // Consol.WriteL
+            // hämtar en lista med data
+            employees = GetData();
+
+            // gör övningarna i tur och ordning och skriv ut varje övning så man ser resultatet
+            
+
+
+            if (!IsNullOrEmpty(employees))
+            {
+                //// E:01  
+                //// 
+                Console.WriteLine("{0} E:01", space);
+                foreach (Employee e in employees)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} ", e.Id, e.FirstName, e.LastName, e.Number);
+                }
+
+
+                //// E:02  
+                Console.WriteLine("{0} E:02", space);
+                //// 
+                var query2 = from Employee in employees
+                            orderby (Employee.FirstName)
+                            select Employee;
+                foreach (Employee e in query2)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} ", e.Id, e.FirstName, e.LastName, e.Number);
+                }
+
+                //// E:03  
+                Console.WriteLine("{0} E:03", space);
+                //// 
+                var query3 = from Employee in employees
+                        orderby (Employee.FirstName)
+                        select Employee;
+                foreach (Employee e in query3)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} ", e.Id, e.FirstName, e.LastName, e.Number);
+                }
+
+                //// E:04  
+                Console.WriteLine("{0} E:04", space);
+                //// 
+                var query4 = employees.OrderBy(e => e.FirstName).Skip(10);
+                foreach (Employee e in query4)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} ", e.Id, e.FirstName, e.LastName, e.Number);
+                }
+
+                //// E:05  
+                Console.WriteLine("{0} E:05", space);
+                //// 
+                var query5 = from e in employees
+                             where e.FirstName == "Where"
+                             select e;
+
+                foreach (Employee e in query5)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} ", e.Id, e.FirstName, e.LastName, e.Number);
+                }
+
+                //// E:06  
+                Console.WriteLine("{0} E:06", space);
+                //// 
+                var query6 = from e in employees
+                             where e.LastName == "Where"
+                             select e;
+
+                foreach (Employee e in query6)
+                {
+                    Console.WriteLine("{0} {1} {2} {3} ", e.Id, e.FirstName, e.LastName, e.Number);
+                }
+
+
+                Console.ReadLine();
+
+            }
+        }
+
         public class Employee
         {
             public int Id { get; set; }
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public int Number { get; set; }
-
-        }
-        static void Main(string[] args)
-        {
-            // Consol.WriteL
-            // hämtar en lista med data
-            var employees = GetData();
-
-            // gör övningarna i tur och ordning och skriv ut varje övning så man ser resultatet
-
-
-
-
-
-            Console.ReadLine();
-
         }
 
         static List<Employee> GetData()
@@ -52,5 +122,11 @@ namespace Linq_ovningar
             e.Add(new Employee() { Id = 12, FirstName = "", LastName = "", Number = 36 });
             return e;
         }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+
     }
 }
